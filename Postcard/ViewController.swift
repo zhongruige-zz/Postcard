ce2327db5f9f81f981ee3073878d868634c2a492
zhongruige <zhongruige@gmail.com>
2015-02-11 21:09:10 -0700

    Third revision, adding Enter Name field functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +28,25 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // updating attributes/properties
-        messageLabel.hidden = false //hidden labels are booleans, true (hidden) or false (not hidden)
-        messageLabel.text = enterMessageTextField.text
         
+        messageLabel.hidden = false
+        //hidden labels are booleans, true (hidden) or false (not hidden)
+        
+        messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
         enterMessageTextField.text = ""
         // clears out the text in the messageTextField; otherwise it would remain.
         // make the keyboard go away
         enterMessageTextField.resignFirstResponder()
+        
+        // Enter Name field functionality
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
